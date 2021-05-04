f0dac444c914089b9ec8642238241e55d45bc145
fabiopiro <piroddifabio01@gmail.com>
2021-05-04 14:41:28 +0200

    Numeri DIspari

--- Snack #5/js/script.js
+++ Snack #5/js/script.js
@@ -5,3 +5,32 @@ Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari (verifichiamolo con una funzione) inseriscilo nell’array.
 Stampa l'array nella console.
 */
+
+// Empty Array
+var array = [];
+
+
+for (i = 0 ; i <= 5 ; i++) {
+
+    var userNumbers = parseInt(prompt("Inserisci un numero"));
+    console.log(userNumbers);
+
+    if (isOdd(userNumbers) == "dispari") {
+        array.push(userNumbers);
+    }
+
+}
+
+console.log(array);
+
+// Functions
+function isOdd(number) {
+
+    if ( (number % 2) == 0) {
+        return "pari"
+    }
+    else {
+        return "dispari"
+    }
+
+}
